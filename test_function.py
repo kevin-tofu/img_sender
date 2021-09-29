86ad95a30140c2479ea0ad266ad849e6139c0821
kohei-tofu <fukouhei001@gmail.com>
2021-09-29 09:45:37 +0900

    add test functions

---
+++ test_function.py
@@ -0,0 +1,12 @@
+
+
+if __name__ == '__main__':
+    
+    from main import send
+    setting = {
+        "url": "",
+        "token": "",
+        "dir_images": "",
+        "recursive": False
+    }
+    send(setting)
